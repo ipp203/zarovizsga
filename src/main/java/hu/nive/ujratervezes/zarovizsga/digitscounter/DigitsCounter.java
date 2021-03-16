32048e00ee669f5ab12db6c8090c6cbd329f4bf5
ipp203 <ipp203@freemail.hu>
2021-03-16 17:37:03 +0100

    zarovizsga - DigitsCounter

--- src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
@@ -1,7 +1,14 @@
 package hu.nive.ujratervezes.zarovizsga.digitscounter;
 
 public class DigitsCounter {
-    public int getCountOfDigits(String s){
-        return 0;
+    public int getCountOfDigits(String s) {
+        if (s == null) {
+            return 0;
+        }
+
+        return (int) s.chars()
+                .distinct()
+                .filter(Character::isDigit)
+                .count();
     }
 }
